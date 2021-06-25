1baaf8c2be4d82f099dfe248dfa6fafb2439d97b
Henry Desroches <yo@henry.codes>
2021-06-25 17:39:36 +0200

    🚧  - Simple section content style

--- components/SimpleSection.vue
+++ components/SimpleSection.vue
@@ -1,7 +1,14 @@
 <template>
-  <section :style="`--background: ${slice.primary.background};`">
-    <h3>Simple Section</h3>
-    <p>{{ JSON.stringify(slice) }}</p>
+  <section
+    class="section-simple"
+    :style="`--background: ${slice.primary.background};`"
+  >
+    <h2 class="section-simple__header">{{ slice.primary.header }}</h2>
+
+    <div
+      class="section-simple__content"
+      v-html="$prismic.asHtml(slice.primary.content)"
+    />
   </section>
 </template>
 
