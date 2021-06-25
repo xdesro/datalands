8c71a9890be8cbefbb0b8510a713072ff827ad00
Henry Desroches <yo@henry.codes>
2021-06-25 18:27:33 +0200

    👠  - Footer first pass

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,3 +1,13 @@
 <template>
-  <footer>Footer</footer>
+  <footer class="footer">
+    <div class="footer__row-top">
+      <span>Footer</span>
+    </div>
+    <div class="footer__row-bottom">
+      <span>Internal</span>
+    </div>
+    <div class="footer__brand">
+      <abbr title="Datalands">DL</abbr>
+    </div>
+  </footer>
 </template>
