aa5e382be8e0cf6b62b81e03f012126984420292
Henry Desroches <yo@henry.codes>
2021-06-25 19:25:59 +0200

    ✨  - Add symbols bar logic

--- components/StatementAnimated.vue
+++ components/StatementAnimated.vue
@@ -1,7 +1,17 @@
 <template>
-  <section :style="`--background: ${slice.primary.background};`">
-    <h3>Statement — Animated</h3>
-    <p>{{ JSON.stringify(slice) }}</p>
+  <section
+    class="statement-animated"
+    :style="`--background: ${slice.primary.background};`"
+  >
+    <h2 class="statement-animated__title">
+      {{ slice.primary.top_text }}
+      <span class="statement-animated__positioned">{{
+        slice.primary.bottom_text
+      }}</span>
+    </h2>
+    <footer class="statement-animated__footer">
+      {{ slice.primary.footer }}
+    </footer>
   </section>
 </template>
 
