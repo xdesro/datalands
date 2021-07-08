3788fef14c5d7d3134c3e2db5c3db265c270dbd0
Henry Desroches <yo@henry.codes>
2021-07-08 14:33:34 -0400

    👁  - Fix client-side rendering of Marquee element

--- components/Marquee.vue
+++ components/Marquee.vue
@@ -1,33 +1,21 @@
 <template>
   <section class="marquee" style="--drift: 0">
-    <!-- <vue-loop-scroll
-      class="marquee__inner"
-      direction="left"
-      :speed="15"
-      :index="0"
-      :mouse-stop="false"
-    > -->
-    <client-only>
-      <marquee-el class="marquee__inner">
-        <span
-          v-for="({ element }, index) in slice.items"
-          :key="`${element}${index}`"
-          class="marquee__tag"
-        >
-          {{ element }}
-        </span>
-        <!-- </vue-loop-scroll> -->
-      </marquee-el>
-    </client-only>
+    <MarqueeEl class="marquee__inner">
+      <span
+        v-for="({ element }, index) in slice.items"
+        :key="`${element}${index}`"
+        class="marquee__tag"
+      >
+        {{ element }}
+      </span>
+    </MarqueeEl>
   </section>
 </template>
 
 <script>
 export default {
   name: 'Marquee',
-  components: {
-    'marquee-el': () => import('@/components/MarqueeEl.vue'),
-  },
+  components: {},
   props: {
     slice: Object,
   },
--- components/Partners.vue
+++ components/Partners.vue
@@ -6,21 +6,18 @@
     <div class="partners__inner">
       <h2 class="partners__title">{{ slice.primary.main }}</h2>
     </div>
-    <client-only>
-      <marquee-el reverse :duration="30">
-        <div class="partners__slider">
-          <div
-            v-for="({ client_logo: { url, alt } }, index) in [...slice.items]"
-            :key="`${alt}${index}`"
-            class="partners__logo-container"
-          >
-            <img :src="url" class="partners__logo" />
-          </div>
+
+    <MarqueeEl reverse :duration="30">
+      <div class="partners__slider">
+        <div
+          v-for="({ client_logo: { url, alt } }, index) in [...slice.items]"
+          :key="`${alt}${index}`"
+          class="partners__logo-container"
+        >
+          <img :src="url" class="partners__logo" />
         </div>
-      </marquee-el>
-    </client-only>
-    <!-- <div class="partners__slider"> -->
-    <!-- </div> -->
+      </div>
+    </MarqueeEl>
   </section>
 </template>
 
@@ -29,9 +26,6 @@ export default {
   name: 'Partners',
   props: {
     slice: Object,
-  },
-  components: {
-    'marquee-el': () => import('@/components/MarqueeEl.vue'),
   },
 }
 </script>
