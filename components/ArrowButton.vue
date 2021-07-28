5b5e9933069afd8b9b06b16e8800170f2a541c15
Henry Desroches <yo@henry.codes>
2021-07-28 10:33:51 -0400

    Add stroke to counteract pixel border effect on buttons

--- components/ArrowButton.vue
+++ components/ArrowButton.vue
@@ -2,6 +2,8 @@
   <svg
     viewBox="0 0 72 56"
     fill="currentColor"
+    stroke="currentColor"
+    stroke-width=".5"
     xmlns="http://www.w3.org/2000/svg"
   >
     <g class="pixels-to-step">
